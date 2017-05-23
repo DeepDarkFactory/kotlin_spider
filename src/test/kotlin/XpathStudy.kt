00c88f8b5a03dfd1a378b6264e14482c67c7b1c5
-A- <1157988103@qq.com>
2017-05-23 16:52:44 +0800

    Yes , I am learning xpath syntax :cactus:

--- src/test/kotlin/XpathStudy.kt
+++ src/test/kotlin/XpathStudy.kt
@@ -8,25 +8,32 @@ import javax.xml.xpath.XPathFactory
  * Created by nbsaw on 2017/5/23.
  */
 
+// find absolute path in resources
 fun getPath(path:String) :String{
     val loader = Thread.currentThread().contextClassLoader
     return loader.getResource(path).path
 }
 
 fun main(args:Array<String>){
+    // init dom instance
     val factory = DocumentBuilderFactory.newInstance()
     factory.isNamespaceAware = true // never forget this!
     val builder = factory.newDocumentBuilder()
     val doc = builder.parse(getPath("books.xml"))
-    // XML
+
+    // init xml instance
     val xmlFactory = XPathFactory.newInstance()
     val xpath = xmlFactory.newXPath()
-    val expr = xpath.compile("//book[author='Neal Stephenson']/title/text()")
+
+    // XPath expr
+    val expr = xpath.compile("/html//book//title")
     var result = expr.evaluate(doc, XPathConstants.NODESET)
     var nodes = result as NodeList
+
+    // loop
     var count = 0
     while (true){
-        var item: Node? = nodes.item(count++) ?: break
-        println(item!!.nodeValue)
+        var item: Node = nodes.item(count++) ?: break
+        println("${item.textContent}")
     }
 }
